22e03b2f223bbc69b490cb561ada4c50e4893d84
Ana Izabel Jacobsen <jacobsenanaizabel@gmail.com>
2019-10-16 09:57:29 -0300

    request for imagens

--- src/services/Services.js
+++ src/services/Services.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-function useFetch(url, secondUrl) {
+function useFetch(url) {
   const [data, setData] = useState([]);
   const [resultDatas, setResults] = useState([]);
 
@@ -10,30 +10,28 @@ function useFetch(url, secondUrl) {
   async function fetchUrl() {
     try {
       const response = await fetch(url);
-      const result = await response.json();
+      const json = await response.json();
+      setData(json)
 
-      if (result) {
-        setResults(result.results);
-        const results = resultDatas;
+      if (json) {
+        setResults(json.results);
+        let results = json.results;
 
-        results.map(async (result) => {
-          const url2 = `https://www.googleapis.com/customsearch/v1?q=${result.name}&cx=006556936853971270487%3Atip6rjdt3xu&searchType=image&key=AIzaSyCV5v1NbhR02akfxiH1LpvOZWcczCMwNWY`
-          const response2 = await fetch(url2);
-          const json2 = await response2.json();
-          const img = json2.items[0].image.thumbnailLink;
-          result.img = img
+        const resultObj = json.results.map(async (result) => {
+          let url = `https://www.googleapis.com/customsearch/v1?q=${result.name}&cx=006556936853971270487%3Atip6rjdt3xu&searchType=image&key=AIzaSyCV5v1NbhR02akfxiH1LpvOZWcczCMwNWY`
+          let response = await fetch(url);
+          let json = await response.json();
+          const img = json ? json.items[0].image.thumbnailLink : { img: "" };
+          results = { ...result, img }
 
+          return {
+            results
+          }
         });
-        setResults(results);
+
+        setResults(resultObj);
         setLoading(false);
       }
-      /*
-      if (secondUrl) {
-        const response2 = await fetch(secondUrl);
-        const json2 = await response2.json();
-        const img = json2.items[0].image.thumbnailLink;
-        setImg(img);
-      }*/
     } catch (error) {
       setErrors(error);
       throw new Error(error);
